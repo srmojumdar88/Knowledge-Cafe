a94dda1a9c3a756d39f8bb537aa2e6eb6a40134d
srmojumdar <rimonmojumdar88@gmail.com>
2023-09-18 17:03:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Blogs from './component/Blogs/blogs'
+import Header from './component/header/header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+
+      <Blogs></Blogs>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/component/header/header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className="text-4xl font-bold bg-orange-400">Knowledge cafe</h2>
+        </div>
+    );
+};
+
+export default Header;
